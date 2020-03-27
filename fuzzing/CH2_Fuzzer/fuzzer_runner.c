65349ce1164330d224f52e0fc292d69e2fa1d0ce
hyorm <hyorm@gmail.com>
2020-03-28 01:58:48 +0900

    modified

--- fuzzing/CH2_Fuzzer/fuzzer_runner.c
+++ fuzzing/CH2_Fuzzer/fuzzer_runner.c
@@ -63,36 +63,18 @@ ReturnRunner run_PrintRunner(PrintRunner rnr, char* inp);
 ReturnRunProcess run_process_Program(ProgramRunner rnr, char* inp);
 ReturnRunProcess run_Program(ProgramRunner rnr, char* inp);
 
-void file_fuzzer(char* file_name);
-
-char* fuzzer(int max_length, int char_start, int char_range);
-
 char* buffer;
-char* fuzzer_output;
-
-char fuzzer_read_input[BUFF_SIZE];
-
-int max_ = 1000, start_ = (int)'a', range_ = 26;//100, 32, 32
 
 int main(int argv, char** argc){
 
-	/*
-	//printf("fuzzer output: %s\n", fuzzer(max_, start_, range_));
-	printf("fuzzer output: %s\n", fuzzer(max_, (int)'a', 26));
-	free(buffer);
-	*/
-	
-	//file_fuzzer("input.txt");
-
-	
 	Runner runner_base = {"PASS", "FAIL", "UNRESOLVED", run_Runner};
-	/*
-	ReturnRunner rur = runner_base.run(runner_base, fuzzer(max_, (int)'a', 26));
+
+	ReturnRunner rur = runner_base.run(runner_base, "Some Input");
 	printf("%s === %s\n", rur.inp, rur.value);
 
 	PrintRunner runner_print = {runner_base, run_PrintRunner};
 	ReturnRunner rur_p = runner_print.run(runner_print, "Some Input");
-	*/
+
 	ProgramRunner runner_process = {runner_base, "echo", run_process_Program, run_Program};
 	ReturnRunProcess rur_rp = runner_process.run(runner_process, "hello world");
 
@@ -178,52 +160,4 @@ ReturnRunner run_PrintRunner(PrintRunner rnr, char* inp){
 	rr.value = rnr.prRnr.unresolved;
 
 	return rr;
-}
-
-void file_fuzzer(char* file_name){
-
-	FILE* fp;
-	char buff[BUFF_SIZE];
-
-	if(0<(fp = fopen(file_name, "w"))){
-		fuzzer_output = fuzzer(max_, (int)'a', 26);
-		//printf("fuzzer_output %s\n", fuzzer_output);
-		free(buffer);
-		fprintf(fp, fuzzer_output,strlen(fuzzer_output));
-		fclose(fp);
-	}
-	else{
-		printf("file open error");
-	}
-
-	if(0<(fp = fopen(file_name, "r"))){	
-		while(fgets(fuzzer_read_input, sizeof(fuzzer_read_input), fp)){
-			//printf("fuzzer_file_input %s\n", fuzzer_read_input);
-		}
-
-		fclose(fp);
-	}
-	else{
-		printf("file open error");
-	}
-
-	assert(strcmp(fuzzer_output, fuzzer_read_input)==0);
-
-	free(buffer);
-	free(fuzzer_output);
-}
-
-char* fuzzer(int max_length, int char_start, int char_range){
-
-	srand((unsigned int)time(NULL));
-
-	int string_length = rand()%(max_length+1);
-
-	char* out = malloc(sizeof(char)*string_length);
-
-	for(int i = 0; i<string_length; i++){
-		char new_out= rand()%char_range+char_start;
-		out[i] = new_out;
-	}
-	return out;
 }

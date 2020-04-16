126ac81e88e8dd1b5bf764380127eebb90410e4a
hyorm <hyorm@gmail.com>
2020-04-16 12:31:10 +0900

    edit

--- fuzzing/example/bc.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(int argc, char** argv){
-
-	if(argc > 2)
-		printf("%d\n",atoi(argv[1])+atoi(argv[2]));
-	else
-		fprintf(stderr, "parsing error\n");
-}
